b48ac59a8b247921e5158efab404d6d6414e0249
John Briggs <sirbriggs@gmail.com>
2018-06-06 14:12:26 -0500

    init commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,88 @@
+import webpack from 'webpack';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const ROOT_PATH = path.resolve(__dirname);
+const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+
+const IS_PROD = process.env.NODE_ENV === 'production'
+console.log('IS_PROD', IS_PROD)
+
+export default {
+  // context: ROOT_PATH + '/app',
+  entry: {
+    main: [
+      'babel-polyfill',
+      // 'react-hot-loader/patch',
+      // 'webpack-hot-middleware/client',
+      path.resolve(SRC_PATH, 'index.js'),
+    ]
+  },
+  output: {
+    path: BUILD_PATH,
+    publicPath: '/dist',
+    filename: '[name].[hash].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /node_modules/,
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.[chunkhash].css',
+      disable: false,
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      template: path.resolve(SRC_PATH, 'index.html'),
+      filename: 'index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [
+          // 'react-hot-loader/webpack',
+          'babel-loader'
+        ]
+      },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+      //   use: 'file-loader'
+      // },
+      {
+        test: /\.css$/,
+        use: [
+          // 'css-hot-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            // options: {
+              // importLoaders: 2
+              // modules: true,
+              // localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+            // }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+};
